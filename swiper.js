6ca1f43ee250cb25fe234f260630b00bb014323a
Dariana <daricorreamedina@gmail.com>
2024-01-13 21:08:06 -0500

    Add tags functionality (#14)
    * Add stop and resume animation effect.
    
    * Fix stop and replay animation effect.
    
    ---------
    
    Co-authored-by: Daridjcm <daricorreamedina@gmail.com132844413+Daridjcm@users.noreply.github.com>

--- swiper.js
+++ swiper.js
@@ -1,12 +1,14 @@
+// Declaration of mySwiper for the scope in the function.
+let mySwiper;
+
 document.addEventListener('DOMContentLoaded', function () {
-  let mySwiper = new Swiper('.mySwiper', {
+  mySwiper = new Swiper('.mySwiper', {
     slidesPerView: 2,
     spaceBetween: 60,
     breakpoints: {
       375: {
         slidesPerView: 3,
       },
-
       640: {
         slidesPerView: 4,
       },
@@ -14,12 +16,23 @@ document.addEventListener('DOMContentLoaded', function () {
         slidesPerView: 5,
       },
     },
-
     effect: 'slide',
     loop: true,
     autoplay: {
       delay: 1000,
       disableOnInteraction: false,
     },
+  });
+
+  const swiperContainer = document.querySelector('.mySwiper');
+
+  // Stop animation
+  swiperContainer.addEventListener('mouseenter', function () {
+    mySwiper.autoplay.stop();
+  });
+
+  // Replay animation
+  swiperContainer.addEventListener('mouseleave', function () {
+    mySwiper.autoplay.start();
   });
 });
