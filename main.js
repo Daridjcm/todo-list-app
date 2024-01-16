dd6a480c931a874d0cb89bf22536df314d156667
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-01-15 21:56:03 -0400

    Add function to create and store notes in localStorage (#23)

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('main.js');
+/**
+ * Función para crear notas almacenándolas en el localStorage
+ * @param {string} title
+ * @param {string} description
+ * @param {date} date
+ */
+function createNote(title, description, date) {
+  const note = {
+    title: title,
+    description: description,
+    date: date,
+  };
+
+  const lastNotes = JSON.parse(localStorage.getItem('notes') ?? '[]');
+
+  const noteObjectString = JSON.stringify([...lastNotes, note]);
+
+  localStorage.setItem('notes', noteObjectString);
+}
