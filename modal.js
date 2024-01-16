004c45d146b0efdfb87d11e57486d6f9c9897ed3
Daridjcm <daricorreamedina@gmail.com>
2024-01-15 22:43:52 -0500

    Format documents with Prettier

--- modal.js
+++ modal.js
@@ -4,7 +4,7 @@ let editDialog = document.getElementById('editDialog');
 
 function openDialog() {
   editDialog.showModal();
-  
+
   // Obtener la fecha actual y mostrarla en el campo datetime-local
   var dateNow = new Date();
   var dateFormat = dateNow.toISOString().slice(0, 16);
@@ -12,21 +12,20 @@ function openDialog() {
 }
 
 function closeDialog() {
-  editDialog.close();  
+  editDialog.close();
 }
-
 
 function editElement() {
   var title = document.getElementById('title').value;
   var description = document.getElementById('description').value;
   var dateNow = document.getElementById('dateNow').value;
 
-  if (title, description, dateNow || value) {
+  if ((title, description, dateNow || value)) {
     console.log('Title: ' + title);
     console.log('Description: ' + description);
     console.log('Current date: ' + dateNow);
   } else {
-    alert("Please, fill outs all fields.");
+    alert('Please, fill outs all fields.');
   }
   // Cerrar el diálogo después de la edición
   cerrarDialog();
