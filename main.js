c5076da46ab40db3cdd56de56ae4c65850edb3a2
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-01-16 22:48:27 -0400

    Refactor card rendering and add notes functionality

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+let notesContainer = document.getElementById('notesContainer');
+let notes = obtenerNotas();
+
+notes.forEach((note) => {
+  const date = new Date(note.date);
+  notesContainer.innerHTML += `
+  <div class="card bg-slate-100 border border-black shadow-lg rounded-2xl p-5 m-3 hover:shadow-xl transition-all">
+  <h3 class="font-bold mb-2">${note.title}</h3>
+  <p class="font-light mb-4">${date.toLocaleString('en-US', { day: '2-digit', month: 'numeric', year: '2-digit' })}</p>
+  <p class="font-light">
+    ${note.description}
+  </p>
+</div>
+  `;
+});
+
 /**
  * Función para crear notas almacenándolas en el localStorage
  * @param {string} title
@@ -24,7 +40,7 @@ function createNote(title, description, date) {
  * @returns {Array} notas
  */
 function obtenerNotas() {
-  const notas = JSON.parse(localStorage.getItem('notas') ?? '[]');
+  const notas = JSON.parse(localStorage.getItem('notes') ?? '[]');
 
   return notas;
 }
