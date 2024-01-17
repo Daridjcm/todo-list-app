aede8e8bf476799db510966fc125444edbc693f5
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-01-16 22:55:47 -0400

    Refactor code to add notes to DOM

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 let notesContainer = document.getElementById('notesContainer');
-let notes = obtenerNotas();
 
-notes.forEach((note) => {
-  const date = new Date(note.date);
-  notesContainer.innerHTML += `
-  <div class="card bg-slate-100 border border-black shadow-lg rounded-2xl p-5 m-3 hover:shadow-xl transition-all">
-  <h3 class="font-bold mb-2">${note.title}</h3>
-  <p class="font-light mb-4">${date.toLocaleString('en-US', { day: '2-digit', month: 'numeric', year: '2-digit' })}</p>
-  <p class="font-light">
-    ${note.description}
-  </p>
-</div>
-  `;
-});
+AgregarNotasAlDOM();
+
+function AgregarNotasAlDOM() {
+  let notes = obtenerNotas();
+
+  notes.forEach((note) => {
+    const date = new Date(note.date);
+    notesContainer.innerHTML += `
+    <div class="card bg-slate-100 border border-black shadow-lg rounded-2xl p-5 m-3 hover:shadow-xl transition-all">
+    <h3 class="font-bold mb-2">${note.title}</h3>
+    <p class="font-light mb-4">${date.toLocaleString('en-US', { day: '2-digit', month: 'numeric', year: '2-digit' })}</p>
+    <p class="font-light">
+      ${note.description}
+    </p>
+  </div>
+    `;
+  });
+}
 
 /**
  * Función para crear notas almacenándolas en el localStorage
@@ -29,10 +34,20 @@ function createNote(title, description, date) {
 
   const lastNotes = JSON.parse(localStorage.getItem('notes') ?? '[]');
 
-  const noteObjectString = JSON.stringify([...lastNotes, note]);
+  const noteObjectString = JSON.stringify([note, ...lastNotes]);
 
   localStorage.setItem('notes', noteObjectString);
-  console.log('main.js');
+
+  notesContainer.innerHTML =
+    `
+    <div class="card bg-slate-100 border border-black shadow-lg rounded-2xl p-5 m-3 hover:shadow-xl transition-all">
+    <h3 class="font-bold mb-2">${note.title}</h3>
+    <p class="font-light mb-4">${date.toLocaleString('en-US', { day: '2-digit', month: 'numeric', year: '2-digit' })}</p>
+    <p class="font-light">
+      ${note.description}
+    </p>
+  </div>
+    ` + notesContainer.innerHTML;
 }
 
 /**
