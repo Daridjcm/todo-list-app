f032a41c48b41ebadd59fd048a17dd66694ae3e0
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-01-16 22:35:09 -0400

    arreglando algunos cmabios

--- main.js
+++ main.js
@@ -28,3 +28,5 @@ function obtenerNotas() {
 
   return notas;
 }
+
+createNote();
--- modal.js
+++ modal.js
@@ -4,11 +4,6 @@ let editDialog = document.getElementById('editDialog');
 
 function openDialog() {
   editDialog.showModal();
-
-  // Obtener la fecha actual y mostrarla en el campo datetime-local
-  var dateNow = new Date();
-  var dateFormat = dateNow.toISOString().slice(0, 16);
-  document.getElementById('dateNow').value = dateFormat;
 }
 
 function closeDialog() {
@@ -18,15 +13,13 @@ function closeDialog() {
 function editElement() {
   var title = document.getElementById('title').value;
   var description = document.getElementById('description').value;
-  var dateNow = document.getElementById('dateNow').value;
 
-  if ((title, description, dateNow || value)) {
+  if ((title, description)) {
     console.log('Title: ' + title);
     console.log('Description: ' + description);
-    console.log('Current date: ' + dateNow);
   } else {
     alert('Please, fill outs all fields.');
   }
   // Cerrar el diálogo después de la edición
-  cerrarDialog();
+  closeDialog();
 }
